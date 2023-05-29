db38d65ee7651a1deeae4dd1b4144da2f458cd5a
patelgr <dev@gauravpatel.net>
2023-05-29 11:13:01 -0400

    Initial Commit

---
+++ app/src/main/java/app/scircle/idh/IdhApplication.java
@@ -0,0 +1,11 @@
+package app.scircle.idh;
+
+import java.util.Arrays;
+
+public class IdhApplication {
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(args));
+    }
+
+}
